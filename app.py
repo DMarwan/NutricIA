b50996cc0c28fdfe08f38215f9bc493a8765903c
DMarwan <marwan.professionnel@gmail.com>
2019-07-18 12:10:25 +0200

    18/07/2019

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ def tables():
 	return render_template('tables.html')
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0',port=1200, debug =  True)
+     port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
